0e84520de3959f36d7cff2b7e2ffb42fed5fb489
Aji Maulana <mancode700@gmail.com>
2024-01-28 17:18:12 +0700

    Add: Create project && contact

---
+++ js/slider.js
@@ -0,0 +1,34 @@
+
+
+var swiper = new Swiper(".continer-portofolio", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    grabCursor : 'true',
+    centeredSlides: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets : true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        626: {
+            slidesPerView: 2,
+        },
+        921: {
+            slidesPerView: 3,
+        },
+    },
+});
