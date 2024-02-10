df1cc046c5c62f38540cf5198398c99718393327
Aji Maulana <mancode700@gmail.com>
2024-02-10 18:07:07 +0700

    Update: Integration API code blind

---
+++ login/js/login.js
@@ -0,0 +1,64 @@
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const btnLogin = document.getElementById("login");
+const failedLogin = document.getElementById("failed-login");
+const successLogin = document.getElementById("success-login");
+
+function removeTimeOutOnElement(element, duration) {
+  clearTimeout(() => {
+    element.style.display = 'none';
+  }, duration);
+}
+
+function displayElement(element, display, duration) {
+  element.style.display = display;
+  setTimeout(() => {
+    element.style.display = 'none';
+  }, duration);
+}
+
+async function sendDataToAPI() {
+  const dataToSend = {
+    username: username.value,
+    password: password.value
+  };
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataToSend)
+  };
+
+  const apiUrl = "https://api-blind-code.vercel.app/login";
+
+  try {
+    const response = await fetch(apiUrl, requestOptions);
+
+    const DURATION_FAILED_LOGIN_OR_REDIRECT_PAGE = 3000
+    const DURATION_SUCCESS_LOGIN = 2000
+
+    if (!response.ok) {
+      displayElement(failedLogin, 'block', DURATION_FAILED_LOGIN_OR_REDIRECT_PAGE);
+      removeTimeOutOnElement(failedLogin, DURATION_FAILED_LOGIN_OR_REDIRECT_PAGE)
+    } else {
+      displayElement(successLogin, 'block', DURATION_SUCCESS_LOGIN);
+      removeTimeOutOnElement(successLogin, DURATION_SUCCESS_LOGIN)
+
+      setTimeout(() => {
+        window.location.assign("https://mancode77.github.io/frontend-dev-portofolio/");
+      }, DURATION_FAILED_LOGIN_OR_REDIRECT_PAGE);
+     
+      clearTimeout(() => {
+        window.location.assign("https://mancode77.github.io/frontend-dev-portofolio/");
+      }, DURATION_FAILED_LOGIN_OR_REDIRECT_PAGE)
+    }
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+}
+
+btnLogin.addEventListener("click", function () {
+  sendDataToAPI();
+});
---
+++ login/js/register.js
@@ -0,0 +1,75 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const btnRegister = document.getElementById("register");
+const failedRegister = document.getElementById("failed-register");
+const successRegister = document.getElementById("success-register");
+
+function removeTimeOutOnElement(element, duration) {
+  clearTimeout(() => {
+    element.style.display = "none";
+  }, duration);
+}
+
+function displayElement(element, display, duration) {
+  element.style.display = display;
+  setTimeout(() => {
+    element.style.display = "none";
+  }, duration);
+}
+
+async function sendDataToAPI() {
+  const dataToSend = {
+    username: username.value,
+    password: password.value,
+  };
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataToSend),
+  };
+
+  const apiUrl = "https://api-blind-code.vercel.app/register";
+
+  try {
+    const response = await fetch(apiUrl, requestOptions);
+
+    const DURATION_FAILED_REGISTER_OR_REDIRECT_PAGE = 3000;
+    const DURATION_SUCCESS_REGISTER = 2000;
+
+    if (!response.ok) {
+      displayElement(
+        failedRegister,
+        "block",
+        DURATION_FAILED_REGISTER_OR_REDIRECT_PAGE
+      );
+      removeTimeOutOnElement(
+        failedRegister,
+        DURATION_FAILED_REGISTER_OR_REDIRECT_PAGE
+      );
+    } else {
+      displayElement(successRegister, "block", DURATION_SUCCESS_REGISTER);
+      removeTimeOutOnElement(successRegister, DURATION_SUCCESS_REGISTER);
+
+      setTimeout(() => {
+        window.location.assign(
+          "https://mancode77.github.io/frontend-dev-portofolio/"
+        );
+      }, DURATION_FAILED_REGISTER_OR_REDIRECT_PAGE);
+
+      clearTimeout(() => {
+        window.location.assign(
+          "https://mancode77.github.io/frontend-dev-portofolio/"
+        );
+      }, DURATION_FAILED_REGISTER_OR_REDIRECT_PAGE);
+    }
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+}
+
+btnRegister.addEventListener("click", function () {
+  sendDataToAPI();
+});
