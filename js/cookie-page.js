52e91710b5158f79417120a207a477264314afc8
Aji Maulana <mancode700@gmail.com>
2024-02-11 18:29:10 +0700

    Update: cookie

--- js/cookie-page.js
+++ js/cookie-page.js
@@ -2,27 +2,30 @@ function checkCookie() {
   const cookieName = "key";
   const myCookie = getCookie(cookieName);
   if (myCookie === "") {
-    // Jika cookie belum ada, arahkan pengguna ke halaman login
-    window.location.href = "http://127.0.0.1:5500/login/login.html";
+    window.location.href = "https://mancode77.github.io/frontend-dev-portofolio/login/login.html";
+  } else {
+    window.location.href = "https://mancode77.github.io/frontend-dev-portofolio/";
   }
 }
 
-// Fungsi untuk mendapatkan nilai cookie
 function getCookie(cookieName) {
   const name = cookieName + "=";
   const decodedCookie = decodeURIComponent(document.cookie);
   const cookieArray = decodedCookie.split(";");
+
   for (let i = 0; i < cookieArray.length; i++) {
     let cookie = cookieArray[i];
+   
     while (cookie.charAt(0) === " ") {
       cookie = cookie.substring(1);
     }
+   
     if (cookie.indexOf(name) === 0) {
       return cookie.substring(name.length, cookie.length);
     }
   }
+
   return "";
 }
 
-// Memeriksa cookie saat halaman dimuat
 window.onload = checkCookie;
--- login/js/cookie.js
+++ login/js/cookie.js
@@ -1,15 +1,21 @@
 export function setCookieExpiration() {
   const date = new Date();
+
   date.setTime(date.getTime() + (30 * 24 * 60 * 60 * 1000)); 
+
   return date.toUTCString();
 }
 
 export function createCookie(name, value, days) {
   let expires = "";
+
   if (days) {
     const expirationDate = new Date();
+
     expirationDate.setTime(expirationDate.getTime() + (days * 24 * 60 * 60 * 1000));
+    
     expires = "; expires=" + expirationDate.toUTCString();
   }
+
   document.cookie = name + "=" + value + expires + "; path=/";
 }
